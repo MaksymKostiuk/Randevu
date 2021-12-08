cc132df55a1e4f6235f08e2170f4b4520a6e17d3
RandevuOriginal <randevuoriginal@gmail.com>
2021-12-07 22:57:07 -0600

    Обновлено Regestration - Personal Case - activity_main.xml

--- app/src/main/java/kost/max/randevu/MainActivity.java
+++ app/src/main/java/kost/max/randevu/MainActivity.java
@@ -3,12 +3,36 @@ package kost.max.randevu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] items = {"Male","Female","No-Gender"};
+    AutoCompleteTextView autoCompleteTxt;
+    ArrayAdapter<String> adapterItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        autoCompleteTxt = findViewById(R.id.auto_complete_txt);
+
+        adapterItems = new ArrayAdapter<>(this,R.layout.list_gender,items);
+
+        autoCompleteTxt.setAdapter(adapterItems);
+
+        autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(),"Item: "+item,Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
